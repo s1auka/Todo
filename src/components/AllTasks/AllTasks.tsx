2a258ba71e39b48c322f4ef1773a7b824c36599f
Svyatoslav <s1aukaa@gmail.com>
2021-05-26 17:06:37 +0300

    ***********

--- src/components/AllTasks/AllTasks.tsx
+++ src/components/AllTasks/AllTasks.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react"
-import { shallowEqual, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { getTasks } from "../../redux/tasks-selector"
 import { Task } from "./Task"
 
 type propsType = {}
 
 export const AllTasks: FC<propsType> = () => {
-    const tasks = useSelector(getTasks, shallowEqual)
+    const tasks = useSelector(getTasks)
 
     return (
         <>
